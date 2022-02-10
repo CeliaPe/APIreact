e951338fc111c06e2dc287451364d488a1bf982d
CeliaPe <celia.pedregosa@itgranada.com>
2022-02-10 08:58:05 +0100

    Ejemplo url POSTS

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import './App.css';
 
 function App() {
   const url ='https://jsonplaceholder.typicode.com/posts'
-  const [todos, setTodos] = useState()
+  const [posts, setPosts] = useState()
 
   const fetchApi = async() =>{
     const respuesta = await fetch(url)
     const respuestaJSON = await respuesta.json()
-    setTodos(respuestaJSON)
+    setPosts(respuestaJSON)
     console.log(respuestaJSON)
   }
 
@@ -19,10 +19,10 @@ function App() {
 
   return (
     <div className="App">
-    <h1>Hola mundo</h1>
-    { !todos ? 'Cargando...' :
-      todos.map( (todo,index) =>{
-        return <li>{todo.title}</li>
+    <h1>Posts</h1>
+    { !posts ? 'Cargando...' :
+      posts.map( (post,index) =>{
+        return <li>{post.title}</li>
       })
     }
     </div>
